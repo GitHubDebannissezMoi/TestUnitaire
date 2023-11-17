abb5a484737a4cb12924bf5c5a1a15d55197e34e
Romain <romainguiffant49@gmail.com>
2023-11-17 15:45:04 +0100

    Struct base

--- Tp_Test/src/main/java/Item.java
+++ Tp_Test/src/main/java/Item.java
@@ -1,2 +1,32 @@
+import java.time.LocalDate;
+
 public class Item {
+    private String name;
+    private char[] content;
+    private LocalDate creationDate;
+
+    public Item(String name,char[] content) {
+        this.name = name;
+        this.content = content;
+        this.creationDate = LocalDate.now();
+
+    }
+
+    public char[] getContent() {
+        return content;
+    }
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setContent(char[] content) {
+        this.content = content;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Tp_Test/src/main/java/ToDoList.java
+++ Tp_Test/src/main/java/ToDoList.java
@@ -1,2 +1,13 @@
+import java.lang.reflect.Array;
+
 public class ToDoList {
+    private Item[] items = new Item[10];
+
+    public Item[] getItems() {
+        return items;
+    }
+
+    public void setItems(Item[] items) {
+        this.items = items;
+    }
 }
